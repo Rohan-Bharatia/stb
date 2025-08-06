98956ba05ab6772ad844b1d5d847c2d1c7c63d9d
Rohan Bharatia <Rohan-Bharatia@github.com>
2025-08-06 12:51:35 -0400

    Add a source file for cmake

---
+++ stb.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#define STB_C_LEXER_IMPLEMENTATION
+#include "stb_c_lexer.h"
+
+#define STB_CONNECTED_COMPONENTS_IMPLEMENTATION
+#include "stb_connected_components.h"
+
+#define STB_DIVIDE_IMPLEMENTATION
+#include "stb_divide.h"
+
+#define STB_DS_IMPLEMENTATION
+#include "stb_ds.h"
+
+#define STB_DXT_IMPLEMENTATION
+#include "stb_dxt.h"
+
+#define STB_EASY_FONT_IMPLEMENTATION
+#include "stb_easy_font.h"
+
+#define STB_HERRINGBONE_WANG_TILE_IMPLEMENTATION
+#include "stb_herringbone_wang_tile.h"
+
+#define STB_HEXWAVE_IMPLEMENTATION
+#include "stb_hexwave.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "stb_image_resize2.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+#define STB_INCLUDE_IMPLEMENTATION
+#include "stb_include.h"
+
+#define STB_LEAKCHECK_IMPLEMENTATION
+#include "stb_leakcheck.h"
+
+#define STB_PERLIN_IMPLEMENTATION
+#include "stb_perlin.h"
+
+#define STB_RECT_PACK_IMPLEMENTATION
+#include "stb_rect_pack.h"
+
+#define STB_SPRINTF_IMPLEMENTATION
+#include "stb_sprintf.h"
+
+#define STB_TEXTEDIT_IMPLEMENTATION
+#include "stb_textedit.h"
+
+#define STB_TILEMAP_EDITOR_IMPLEMENTATION
+#include "stb_tilemap_editor.h"
+
+#define STB_TRUETYPE_IMPLEMENTATION
+#include "stb_truetype.h"
+
+#define STB_VORBIS_IMPLEMENTATION
+#include "stb_vorbis.h"
+
+#define STB_VOXEL_RENDER_IMPLEMENTATION
+#include "stb_voxel_render.h"
